0f44b4839ee39618c5a33254b7897f9c3d81001e
MarianoJSH <mariano.s.hidalgo@gmail.com>
2026-02-12 14:00:54 +0100

    Actualización de URLs en main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   // Crucial para que Angular pueda consumir la API desde cualquier url que apuntemos
   const allowedOrigins = [
     'http://localhost:4200',
-    'https://sentinel-code-ai.vercel.app',
+    'https://sentinel-code-backend.onrender.com',
   ];
 
   app.enableCors({

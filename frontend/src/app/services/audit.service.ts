d52a45134a7085b67ee7a5901a2140ff7436334d
MarianoJSH <mariano.s.hidalgo@gmail.com>
2026-02-16 11:11:49 +0100

    Nuevos cambios en la validación de entrada de código

--- frontend/src/app/services/audit.service.ts
+++ frontend/src/app/services/audit.service.ts
@@ -1,33 +1,53 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 /**
  * Interfaz para definir la estructura de la petición al Backend
  */
 
 interface AuditRequest {
-    code: string;
-    fileName: string;
-    sourceType: 'file' | 'paste'
+  code: string;
+  fileName: string;
+  sourceType: 'file' | 'paste';
+}
+
+/**
+ * Interfaz para definir la estructura de la respuesta del Backend
+ */
+interface AuditResponse {
+  // Definir la estructura de la respuesta del backend
+  result: string;
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuditService {
-    // URL del backend  (ajustada con el prefijo /api)
-    private readonly API_URL = 'https://sentinel-code-backend.onrender.com/api/ai/analyze';
+  // URL del backend  (ajustada con el prefijo /api)
+  private readonly API_URL =
+    'https://sentinel-code-backend.onrender.com/api/ai/analyze';
 
-    constructor (private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {}
 
-    /**
-     * Envía el código al backend para su análisis.
-     * @param data Objeto con el código y metadatos.
-     * @return Observable con la respuesta de la IA.
-     */
+  /**
+   * Envía el código al backend para su análisis.
+   * @param data Objeto con el código y metadatos.
+   * @return Observable con la respuesta de la IA.
+   */
 
-    sendForAudit(data: AuditRequest): Observable<any> {
-        return this.http.post<any>(this.API_URL, data);
-    }
+  sendForAudit(data: AuditRequest): Observable<AuditResponse> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http.post<AuditResponse>(this.API_URL, data, { headers }).pipe(
+      map((response) => response),
+      catchError((error) => {
+        console.error(error);
+        return throwError('Error al enviar la petición');
+      }),
+    );
+  }
 }
